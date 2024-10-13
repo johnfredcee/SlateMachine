5ba55b74df5535704cc8c8e7cc9e6b2d308e2004
John Connors <johnfredcee@gmail.com>
2024-10-13 15:24:42 +0100

    Fill in some basic overrides for the Graph

--- Source/SlateMachine/Private/SlateMachineEdGraph.cpp
+++ Source/SlateMachine/Private/SlateMachineEdGraph.cpp
@@ -1,4 +1,54 @@
 ﻿// // Copyright (C) John Connors 2023
 
 
+#include "SlateMachineSchema.h"
 #include "SlateMachineEdGraph.h"
+
+#include "GraphEditAction.h"
+
+#define LOCTEXT_NAMESPACE "SlateMachineGraph"
+
+DEFINE_LOG_CATEGORY_STATIC(LogSlateMachine, Log, All);
+
+USlateMachineEdGraph::USlateMachineEdGraph() : Super()
+{
+    Schema = USlateMachineSchema::StaticClass();
+    return;
+}
+
+void USlateMachineEdGraph::PostInitProperties()
+{
+    Super::PostInitProperties();
+}
+
+void USlateMachineEdGraph::AddNode(UEdGraphNode* NodeToAdd, bool bUserAction, bool bSelectNewNode)
+{
+    Super::AddNode(NodeToAdd, bUserAction, bSelectNewNode);
+}
+
+FString USlateMachineEdGraph::GetDesc()
+{
+    return TEXT("Slate Machine");
+}
+
+bool USlateMachineEdGraph::IsAsset() const
+{
+    return true;
+}
+
+bool USlateMachineEdGraph::IsEditorOnly() const
+{
+    return false;
+}
+
+void USlateMachineEdGraph::NotifyGraphChanged(const FEdGraphEditAction& Action)
+{
+    Super::NotifyGraphChanged(Action);
+    UE_LOG(LogSlateMachine, Log, TEXT("%05x"), (int) Action.Action);
+}
+
+void USlateMachineEdGraph::RefreshGraph()
+{
+    return;
+}
+#undef LOCTEXT_NAMESPACE
--- Source/SlateMachine/Public/SlateMachineEdGraph.h
+++ Source/SlateMachine/Public/SlateMachineEdGraph.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "SimpleStateMachine.h"
 #include "EdGraph/EdGraph.h"
 #include "SlateMachineEdGraph.generated.h"
 
@@ -13,4 +14,19 @@ UCLASS()
 class SLATEMACHINE_API USlateMachineEdGraph : public UEdGraph
 {
     GENERATED_BODY()
+
+    UPROPERTY()
+    TObjectPtr<USimpleStateMachine> StateMachine;
+    
+public:
+    USlateMachineEdGraph();
+    virtual void PostInitProperties() override;
+    virtual void AddNode(UEdGraphNode* NodeToAdd, bool bUserAction, bool bSelectNewNode) override;
+    virtual FString GetDesc() override;
+    virtual bool IsAsset() const override;
+    virtual bool IsEditorOnly() const override;
+    virtual void NotifyGraphChanged(const FEdGraphEditAction& Action) override;
+
+private:
+    void RefreshGraph();
 };
